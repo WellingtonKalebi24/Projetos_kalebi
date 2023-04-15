bb83d8888caded6e473c9093f0407044eadd75c2
WellingtonKalebi <wellington_rdp@live.com>
2023-04-15 17:55:07 -0300

    Create main.py

---
+++ Calculadora/main.py
@@ -0,0 +1,10 @@
+# importando tkinter biblioteca de interface
+from tkinter import *
+from tkinter import ttk
+
+
+janela = Tk()
+janela.title("Calculadora")
+janela.geometry("280x320")
+
+janela.mainloop()
