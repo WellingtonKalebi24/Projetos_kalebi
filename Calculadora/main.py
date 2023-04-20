7282088f970fd77d21dadfecdceb4049658d12c8
WellingtonKalebi <wellington_rdp@live.com>
2023-04-19 21:48:15 -0300

    2 line calculadora

--- Calculadora/main.py
+++ Calculadora/main.py
@@ -23,20 +23,21 @@ frame_corpo.grid(row=1,column=0)
 
 # criando botoes
 
-b_1 = Button(frame_corpo, text="C", width=18, height=2, bg=cor4)
+b_1 = Button(frame_corpo, text="C", width=18, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 b_1.place(x=0, y=0)
-b_2 = Button(frame_corpo, text="%", width=9, height=2, bg=cor4)
-b_2.place(x=135, y=0)
-b_3 = Button(frame_corpo, text="/", width=9, height=2, bg=cor5, fg=cor2) #fg=cor da fonte 
-b_3.place(x=207, y=0)
+b_2 = Button(frame_corpo, text="%", width=9, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_2.place(x=150, y=0)
+b_3 = Button(frame_corpo, text="/", width=9, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE) #fg=cor da fonte 
+b_3.place(x=220, y=0)
 
-#b_4 = Button(frame_corpo, text="C", width=18, height=2)
-#b_4.place(x=0, y=0)
-#b_5 = Button(frame_corpo, text="%", width=9, height=2)
-#b_5.place(x=135, y=0)
-#b_6 = Button(frame_corpo, text="/", width=9, height=2)
-#b_6.place(x=207, y=0)
-
+b_4 = Button(frame_corpo, text="7", width=8, height=2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_4.place(x=0, y=52)
+b_5 = Button(frame_corpo, text="8", width=8, height=2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_5.place(x=85, y=52)
+b_6 = Button(frame_corpo, text="9", width=6, height=2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_6.place(x=150, y=52)
+b_7 = Button(frame_corpo, text="*", width=9, height=2,bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+b_7.place(x=220, y=52)
 
 
 
